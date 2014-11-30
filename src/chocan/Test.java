72e16cba5605ffa1a3674ac4a236f119b57aafe2
ngough <goughn@students.ipfw.edu>
2014-11-30 12:31:01 -0500

    test commit

---
+++ src/chocan/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chocan;
+
+/**
+ * @author Nate
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Testing Git operations!
+	}
+
+}
